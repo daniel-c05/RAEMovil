2eb90f70d680b05f8bfdaefbf26ea46553e7d391
Daniel Alvarado <everest.deth@gmail.com>
2013-02-25 13:04:15 -0600

    Updated Readme and fixed search key behaviour

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-RAEMovil
+﻿RAEMovil
 ========
 
-Diccionario en linea - Real Academia Española
+<h1>Diccionario en linea - Real Academia Española</h1>
+
+El proyecto fue creado por la falta de opciones actualizadas en el Play Store - las dos unicas opciones que encontré todavia utilizaban elementos de interface pre-ICS.
+
+<h2> - Qué Hace? </h2> 
+
+RAE Móvil te permite buscar una palabra de tu elección en uno de los dos diccionarios disponibles por parte de la Real Academia Española: <i>El Diccionario de la Lengua Española</i> y <i>El Diccionario Panhispánico de Dudas<i>. 
+
+<h2> - Qué la diferencia de las demás? </h2> 
+
+	* Interfaz de usuario limpia y simple, pura de Android. 
+	* Sugerencias basadas en búsquedas anteriores. 
+	* Búsquedas sin internet de términos previamente acesados.
+	* Código público, es decir, puedes tomarte el tiempo de analizar como esta construida la aplicación.
+
+<h2> - Hay algo que no funciona, o algo que te gustaría que la aplicación hiciera? </h2>
+
+Si sabes utilizar github, enviame un pull request con los cambios sugeridos. Si no, no te preocupes, enviame una descripción de los cambios que te gustarían ver y yo haré lo posible para hacerlos realidad pronto. 
+
+Solo entra a Configuración -> Acerca de -> Reporta un problema. 
--- src/com/lazybits/rae/movil/Home.java
+++ src/com/lazybits/rae/movil/Home.java
@@ -3,6 +3,7 @@ package com.lazybits.rae.movil;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -19,6 +20,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager.LayoutParams;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
@@ -163,13 +165,19 @@ public class Home extends Activity implements OnEditorActionListener {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {		
 		
 		if ((keyCode == KeyEvent.KEYCODE_SEARCH)) {
-			searchInput.requestFocus();
-			getWindow().setSoftInputMode(
-					LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+			showSoftKeyboard(searchInput);
 			return true;
 		}
 		
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	public void showSoftKeyboard(View view) {
+	    if (view.requestFocus()) {
+	        InputMethodManager imm = (InputMethodManager)
+	                getSystemService(Context.INPUT_METHOD_SERVICE);
+	        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+	    }
 	}
 
 	@Override
